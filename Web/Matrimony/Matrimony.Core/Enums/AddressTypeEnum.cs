3816b7685753289f4fbcf00653f0c2a083d1b6f5
DESKTOP-O2RGDVD\Rinku <sankirnarana@gmail.com>
2024-05-04 23:50:18 +0530

    no message

---
+++ Web/Matrimony/Matrimony.Core/Enums/AddressTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrimony.Core.Enums
+{
+    public enum AddressTypeEnum : int
+    {
+        Home = 1,
+        Current = 2,
+        Other = 3
+    }
+}
---
+++ Web/Matrimony/Matrimony.Core/Enums/RelationTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrimony.Core.Enums
+{
+    public enum RelationTypeEnum : int
+    {
+        Mother = 1,
+        Father = 2,
+        Brother = 3,
+        Sister = 4,
+        Other = 101
+    }
+}
