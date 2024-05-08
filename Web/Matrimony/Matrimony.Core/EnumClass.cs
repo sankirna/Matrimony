5a95c224b2f002c99040eaaeb144c46e1cb2435c
DESKTOP-O2RGDVD\Rinku <sankirnarana@gmail.com>
2024-05-08 15:29:42 +0530

    no message

---
+++ Web/Matrimony/Matrimony.Core/EnumClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrimony.Core
+{
+    public class EnumClass
+    {
+        public string Key { get; set; }
+        public int Value { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Web/Matrimony/Matrimony.Core/EnumHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+
+namespace Matrimony.Core
+{
+    public static class EnumHelper
+    {
+        // Get the value of the description attribute if the   
+        // enum has one, otherwise use the value.  
+        public static string GetDescription<TEnum>(this TEnum value)
+        {
+            var fi = value.GetType().GetField(value.ToString());
+
+            if (fi != null)
+            {
+                var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attributes.Length > 0)
+                {
+                    return attributes[0].Description;
+                }
+            }
+
+            return value.ToString();
+        }
+
+        private static IEnumerable<EnumClass> ListFor<T>(T t) where T : struct
+        {
+            return Enum.GetValues(typeof(T))
+                       .Cast<Enum>()
+                       .Select(e => new EnumClass { Key = e.ToString(), Value = Convert.ToInt16(e), Description = e.GetDescription() });
+        }
+    }
+}
