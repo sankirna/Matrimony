a4942a7459305b8509af2295d29b4bc28fd4e28e
DESKTOP-O2RGDVD\Rinku <sankirnarana@gmail.com>
2024-05-11 19:09:24 +0530

    no message

--- UX/src/app/Interceptors/Request.interceptor.ts
+++ UX/src/app/Interceptors/Request.interceptor.ts
@@ -1,21 +1,32 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { AuthService } from "../services/authService";
 import { Injectable } from "@angular/core";
-import { Observable, tap } from "rxjs";
+import { Observable, finalize, tap } from "rxjs";
 import { environment } from '../../environments/environment';
+import { LoaderService } from "../services/LoaderService";
 
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
+  constructor(
+    private loaderService: LoaderService
+  ){ }
+  
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loaderService.setLoading(true, request.url);
     let  modifiedReq = request.clone({
         url: environment.apiUrl + request.url,
         setHeaders:{ 'Content-Type': 'application/json' }
       });
     return next.handle(modifiedReq ? modifiedReq : request).pipe(tap(() => {
     },
+    
     (err: any) => {
    
-  }));
+  })).pipe(
+    finalize(() =>{
+      this.loaderService.setLoading(false, request.url);
+    }
+    ) ) 
  }
 }
--- UX/src/app/layout/main/main.component.ts
+++ UX/src/app/layout/main/main.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../../services/LoaderService';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  isLoading: boolean = false;
 
+  constructor(
+    private loaderService: LoaderService
+  ){ }
+
+
+  ngOnInit() {
+    this.listenToLoading();
+  }
+
+  listenToLoading(): void {
+    this.loaderService.loadingSub
+      .pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
+      .subscribe((loading) => {
+        this.isLoading = <boolean>loading;
+      });
+  }
 }
